bcbd767fb8cb2ff85a0e481aaae62f9ce20a213b
Andy Park <atpark333@gmail.com>
2023-02-24 00:06:26 +0900

    refactor: Extract fish rendering logic to method

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,33 @@ import java.awt.*;
 import java.net.URL;
 
 public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Aquarium");
+    private static void renderFish(JPanel panel, Graphics graphics) {
+        try {
+            URL iconPath = Main.class.getClassLoader().getResource("sprites/fishTile_080.png");
+            if (iconPath != null) {
+                ImageIcon fishSprite = new ImageIcon(iconPath);
+                fishSprite.paintIcon(panel, graphics, 80, 80);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Unable to load icon");
+        }
+    }
 
+    public static void main(String[] args) {
+        // Make a panel
         JPanel panel = new JPanel();
         panel.setBounds(0, 0, 600, 600);
         panel.setBackground(Color.blue);
-        frame.add(panel);
 
+        // Make a frame
+        JFrame frame = new JFrame("Aquarium");
+        frame.add(panel);
         frame.setSize(600, 600);
         frame.setLayout(null);
         frame.setVisible(true);
 
-        Graphics gfx = frame.getGraphics();
-        URL iconPath = Main.class.getClassLoader().getResource("sprites/fishTile_080.png");
-
-        try {
-            ImageIcon fishSprite = new ImageIcon(iconPath);
-            fishSprite.paintIcon(panel, gfx, 80, 80);
-        } catch (NullPointerException e) {
-            System.out.println(e.toString());
-            System.out.println("Resource not found at: " + iconPath);
-        }
+        // Add a sprite
+        Graphics graphics = frame.getGraphics();
+        renderFish(panel, graphics);
     }
 }
