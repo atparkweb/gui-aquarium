35dc087490e97d33da02976ec1f03e18411eb9b5
Andy Park <atpark333@gmail.com>
2023-02-24 21:38:28 +0900

    refactor: Extract Fish code from main

--- src/Fish.java
+++ src/Fish.java
@@ -1,18 +1,28 @@
-import javax.swing.ImageIcon;
+import javax.swing.*;
 import java.net.URL;
 
-public class Fish {
+public class Fish implements Drawable {
     private final ImageIcon sprite;
     public int x;
     public int y;
 
-    public Fish(URL spriteURL) throws NullPointerException {
-        this(spriteURL, 80, 80);
+    public Fish() throws NullPointerException {
+        this(80, 80);
     }
 
-    public Fish(URL spriteURL, int x, int y) throws NullPointerException {
-        this.sprite = new ImageIcon(spriteURL);
+    public Fish(int x, int y) throws NullPointerException {
+        final String spritePath = "sprites/fishTile_080.png";
+
+        URL resourceURL = Fish.class.getClassLoader().getResource(spritePath);
+
+        if (resourceURL == null) throw new NullPointerException("Unable to load resource: " + spritePath);
+        this.sprite = new ImageIcon(resourceURL);
         this.x = x; this.y = y;
+    }
+
+    @Override
+    public void render(Window window) {
+        this.sprite.paintIcon(window.panel, window.graphics, this.getX(), this.getY());
     }
 
     public ImageIcon getSprite() { return this.sprite; }
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-import java.net.URL;
 
 public class Main {
-    private static void renderFish(JPanel panel, Graphics graphics) {
-        URL resourceURL = Main.class.getClassLoader().getResource("sprites/fishTile_080.png");
-        Fish fish = new Fish(resourceURL);
-        ImageIcon fishSprite = fish.getSprite();
-        fishSprite.paintIcon(panel, graphics, fish.getX(), fish.getY());
-    }
-
     public static void main(String[] args) {
-        // Make a panel
-        JPanel panel = new JPanel();
-        panel.setBounds(0, 0, 600, 600);
-        panel.setBackground(Color.blue);
-
-        // Make a frame
-        JFrame frame = new JFrame("Aquarium");
-        frame.add(panel);
-        frame.setSize(600, 600);
-        frame.setLayout(null);
-        frame.setVisible(true);
-
-        // Add a sprite
-        Graphics graphics = frame.getGraphics();
-        renderFish(panel, graphics);
+        Window window = Window.getInstance();
+        Fish fish = new Fish();
+        fish.render(window);
     }
 }
