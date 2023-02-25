4ce57cb65d0e0ee35b283c8be54a9f03dd2ff35e
Andy Park <atpark333@gmail.com>
2023-02-25 16:02:09 +0900

    refactor: Simplify main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Window window = Window.getInstance();
+
         Renderer renderer = new Renderer(window);
 
-        Fish fish = new Fish();
-        renderer.render(fish);
+        renderer.render(new Fish());
     }
 }
