b1459937887f8681b72c51c729d4c2b71630d9e1
Andy Park <atpark333@gmail.com>
2023-02-27 20:51:32 +0900

    feat: Implement move method and add velocity

--- src/Fish.java
+++ src/Fish.java
@@ -5,10 +5,8 @@ public class Fish implements Sprite {
     ImageIcon icon;
     private int x;
     private int y;
-
-    public Fish() {
-        this(80, 80);
-    }
+    private int vx = 1;
+    private int vy = 1;
 
     public Fish(int x, int y) throws NullPointerException {
         final String iconPath = "sprites/fishTile_080.png";
@@ -16,21 +14,22 @@ public class Fish implements Sprite {
         URL resourceURL = Fish.class.getClassLoader().getResource(iconPath);
 
         if (resourceURL == null) throw new NullPointerException("Unable to load resource: " + iconPath);
-        this.icon = new ImageIcon(resourceURL);
+        icon = new ImageIcon(resourceURL);
         this.x = x; this.y = y;
     }
 
-    public void move(int x, int y) {
-        this.x = x;
-        this.y = y;
+    @Override
+    public void move() {
+        x += vx;
+        y += vy;
     }
 
     @Override
-    public ImageIcon getIcon() { return this.icon; }
+    public ImageIcon getIcon() { return icon; }
 
     @Override
-    public int getX() { return this.x; }
+    public int getX() { return x; }
 
     @Override
-    public int getY() { return this.y; }
+    public int getY() { return y; }
 }
--- src/Piranha.java
+++ src/Piranha.java
@@ -5,6 +5,8 @@ public class Piranha implements Sprite {
     ImageIcon icon;
     private int x;
     private int y;
+    private int vx = 1;
+    private int vy = 1;
 
     public Piranha(int x, int y) throws NullPointerException {
         final String iconPath = "sprites/fishTile_072.png";
@@ -12,22 +14,22 @@ public class Piranha implements Sprite {
         URL resourceURL = Piranha.class.getClassLoader().getResource(iconPath);
 
         if (resourceURL == null) throw new NullPointerException("Unable to load resource: " + iconPath);
-        this.icon = new ImageIcon(resourceURL);
+        icon = new ImageIcon(resourceURL);
         this.x = x; this.y = y;
     }
 
     @Override
-    public int getX() {
-        return this.x;
+    public void move() {
+        x += vx;
+        y += vy;
     }
 
     @Override
-    public int getY() {
-        return this.y;
-    }
+    public ImageIcon getIcon() { return icon; }
 
     @Override
-    public ImageIcon getIcon() {
-        return this.icon;
-    }
+    public int getX() { return x; }
+
+    @Override
+    public int getY() { return y; }
 }
