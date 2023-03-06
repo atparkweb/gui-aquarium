d4a821cdd0eca8971a892aaede44831806186b4c
Andy Park <atpark333@gmail.com>
2023-03-06 22:32:41 +0900

    feat: Add clock class

---
+++ src/Clock.java
@@ -0,0 +1,34 @@
+import javax.swing.Timer;
+
+public class Clock {
+    public interface Callback {
+        void tick(Clock clock);
+    }
+
+    private Timer timer;
+
+    private Callback callback;
+
+    public void setCallback(Callback tick) {
+        this.callback = tick;
+    }
+
+    public void start() {
+        if (timer == null) {
+            timer = new Timer(5, e -> {
+                if (callback != null) {
+                    callback.tick(Clock.this);
+                }
+            });
+
+            timer.start();
+        }
+    }
+
+    public void stop() {
+        if (timer != null) {
+            timer.stop();
+            timer = null;
+        }
+    }
+}
