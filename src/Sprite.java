ec1ed181e072f15b8ab1d0834e1fee42a752de9a
Andy Park <atpark333@gmail.com>
2023-03-08 23:27:20 +0900

    refactor: Add dimensions to sprite
    Purpose: to include image width and height in animal movement boundaries.

--- src/Sprite.java
+++ src/Sprite.java
@@ -4,16 +4,17 @@ import java.net.URL;
 public class Sprite {
     private ImageIcon icon;
     private String iconPath;
+    private int width;
+    private int height;
 
     public Sprite(String iconPath) {
-        this.iconPath = iconPath;
-        loadIcon();
+        updateIcon(iconPath);
     }
 
-    private void loadIcon() throws NullPointerException {
+    private ImageIcon loadIcon() throws NullPointerException {
         URL resourceURL = Sprite.class.getClassLoader().getResource(iconPath);
         if (resourceURL == null) throw new NullPointerException("Unable to load resource: " + iconPath);
-        icon = new ImageIcon(resourceURL);
+        return new ImageIcon(resourceURL);
     }
 
     public ImageIcon getIcon() {
@@ -22,6 +23,11 @@ public class Sprite {
 
     public void updateIcon(String iconPath) {
         this.iconPath = iconPath;
-        loadIcon();
+        icon = loadIcon();
+        width = icon.getIconWidth();
+        height = icon.getIconHeight();
     }
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
 }
