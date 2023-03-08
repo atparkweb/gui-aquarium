55ce00037e1c6bdffc15ec415761d7014a4f6056
Andy Park <atpark333@gmail.com>
2023-03-08 23:36:56 +0900

    refactor: Get animals from spawning pool

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Animal fish = new Animal(80, 80, 2, 1, AnimalType.FISH);
-        Animal piranha = new Animal(80, 240, 1, 2, AnimalType.PIRANHA);
-        ArrayList<Animal> animals = new ArrayList<>();
-        animals.add(fish);
-        animals.add(piranha);
+        ArrayList<Animal> animals = SpawningPool.getAnimals();
 
         EventQueue.invokeLater(() -> {
             JFrame frame = Window.getInstance();
