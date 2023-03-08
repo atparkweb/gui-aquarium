05cf4ca23a05fcf470b760828cde06555d6b0579
Andy Park <atpark333@gmail.com>
2023-03-08 23:25:46 +0900

    feat: Add frame singleton

---
+++ src/Window.java
@@ -0,0 +1,32 @@
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+    private static Window instance;
+    private final int width;
+    private final int height;
+
+    private Window() {
+        this(800, 600);
+    }
+
+    private Window(int width, int height) {
+        super();
+        this.width = width;
+        this.height = height;
+        this.setTitle("GUI Aquarium");
+        this.setResizable(false);
+        this.setFocusable(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+
+    public static Window getInstance() {
+        if (instance == null) {
+            instance = new Window();
+        }
+
+        return instance;
+    }
+}
