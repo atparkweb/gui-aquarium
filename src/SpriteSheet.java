38172b4d9844e00eda778dbcceed66d1bd12fa99
Andy Park <atpark333@gmail.com>
2023-03-11 23:36:49 +0900

    feat: Update constructor and add dimension accessors

--- src/SpriteSheet.java
+++ src/SpriteSheet.java
@@ -2,15 +2,24 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class SpriteSheet {
     private BufferedImage sheetImage;
 
     public SpriteSheet(String path) {
         try {
-            sheetImage = ImageIO.read(new File(path));
-        } catch(IOException ex) {
+            URL resource = SpriteSheet.class.getResource(path);
+            if (resource == null) throw new NullPointerException("Unable to get resource from path: " + path);
+
+            File f = Paths.get(resource.toURI()).toFile();
+
+            sheetImage = ImageIO.read(f);
+        } catch(IOException | URISyntaxException | NullPointerException ex) {
             System.out.println(ex.getMessage());
+            System.exit(1);
         }
     }
 
@@ -22,5 +31,21 @@ public class SpriteSheet {
     // TODO Make method that can get any sprite given a label and resolution
     public BufferedImage getPiranhaSprite() {
         return sheetImage.getSubimage(320, 428, 54, 38);
+    }
+
+    public static int getSpriteWidth(AnimalType type) {
+        switch (type) {
+            case FISH: return 54;
+            case PIRANHA: return 54;
+            default: return 0;
+        }
+    }
+
+    public static int getSpriteHeight(AnimalType type) {
+        switch (type) {
+            case FISH: return 30;
+            case PIRANHA: return 38;
+            default: return 0;
+        }
     }
 }
