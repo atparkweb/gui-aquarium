b968175f843caddfc3cc025586ae977c770dff27
Andy Park <atpark333@gmail.com>
2023-03-12 15:06:14 +0900

    feat: Add data structure

---
+++ src/SpriteProps.java
@@ -0,0 +1,11 @@
+public class SpriteProps {
+    int x; int y;
+    int width; int height;
+
+    public SpriteProps(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
