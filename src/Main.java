a395cad44ee1a1ada796b41011924f64cd25fb60
Andy Park <atpark333@gmail.com>
2023-03-25 15:43:01 +0900

    refactor: Remove unnecessary event queue

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
 import atparkweb.graphics.Renderer;
 import atparkweb.graphics.Window;
-
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            JFrame frame = Window.getInstance();
-            Renderer renderer = new Renderer(30, frame.getWidth(), frame.getHeight());
-            frame.add(renderer);
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-        });
+        JFrame frame = Window.getInstance();
+        Renderer renderer = new Renderer(30, frame.getWidth(), frame.getHeight());
+        frame.add(renderer);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
