6d29aff1abee1bb9b20cb6b05f33721befccce55
Andy Park <atpark333@gmail.com>
2023-04-03 00:59:56 +0900

    feature: Add drawable interface

---
+++ src/atparkweb/animation/Drawable.java
@@ -0,0 +1,17 @@
+package atparkweb.animation;
+
+public class Drawable {
+    protected int x;
+    protected int y;
+
+    public Drawable(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+}
